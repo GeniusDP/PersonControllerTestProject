105e05f84779c0df7b64a96fb582064ab077b48d
GeniusDP <zaranikbz@gmail.com>
2022-11-17 22:21:02 +0200

    getAllPeople() endpoint testing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 insert into people (first_name, last_name, date_of_birth)
-values ('Bogdan', 'Zaranik', '2002-07-24');
+values ('Bogdan', 'Zaranik', '2002-07-24'),
+       ('Maxim', 'Borisov', '2002-05-11');
--- src/test/java/com/example/testproject/TestProjectApplicationTests.java
+++ src/test/java/com/example/testproject/TestProjectApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.testproject;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = TestProjectApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.yaml")
 class TestProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+  void getAllPeople() throws Exception {
+    mvc.perform(
+        MockMvcRequestBuilders.get("/api/person-management/people")
+          .contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(content()
+        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$[0].firstName").value("Bogdan"))
+      .andExpect(jsonPath("$[0].lastName").value("Zaranik"))
+      .andExpect(jsonPath("$[1].firstName").value("Maxim"))
+      .andExpect(jsonPath("$[1].lastName").value("Borisov"));
+  }
 
 }
