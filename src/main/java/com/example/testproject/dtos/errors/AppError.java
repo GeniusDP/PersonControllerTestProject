73b6e9f4e9055f0038a5d24a1631af026481cbee
GeniusDP <zaranikbz@gmail.com>
2022-11-17 22:26:31 +0200

    refactored AppError.java: description changed to value

--- src/main/java/com/example/testproject/dtos/errors/AppError.java
+++ src/main/java/com/example/testproject/dtos/errors/AppError.java
@@ -2,7 +2,7 @@ package com.example.testproject.dtos.errors;
 
 import java.time.LocalDateTime;
 
-public record AppError(String description, LocalDateTime causedTime) {
+public record AppError(String value, LocalDateTime causedTime) {
 
   public static AppError justNow(String message) {
     return new AppError(message, LocalDateTime.now());
