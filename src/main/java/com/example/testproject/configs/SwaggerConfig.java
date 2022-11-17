53da6d2d291fe7ab8c5b3573cc2ccc173ce50099
GeniusDP <zaranikbz@gmail.com>
2022-11-17 12:52:39 +0200

    swagger config indentation

--- src/main/java/com/example/testproject/configs/SwaggerConfig.java
+++ src/main/java/com/example/testproject/configs/SwaggerConfig.java
@@ -10,31 +10,31 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-    @Value("${spring.application.name}")
-    private String appName;
+  @Value("${spring.application.name}")
+  private String appName;
 
-    @Value("${spring.application.version}")
-    private String appVersion;
+  @Value("${spring.application.version}")
+  private String appVersion;
 
-    @Value("${spring.application.description}")
-    private String appDescription;
+  @Value("${spring.application.description}")
+  private String appDescription;
 
-    @Value("${springdoc.contact.name}")
-    private String contactName;
+  @Value("${springdoc.contact.name}")
+  private String contactName;
 
-    @Value("${springdoc.contact.email}")
-    private String contactEmail;
+  @Value("${springdoc.contact.email}")
+  private String contactEmail;
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title(appName)
-                        .version(appVersion)
-                        .description(appDescription)
-                        .contact(new Contact()
-                        .name(contactName)
-                        .email(contactEmail))
-                );
-    }
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+      .info(new Info()
+        .title(appName)
+        .version(appVersion)
+        .description(appDescription)
+        .contact(new Contact()
+          .name(contactName)
+          .email(contactEmail))
+      );
+  }
 }
